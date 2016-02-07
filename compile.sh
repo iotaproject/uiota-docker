e7a55a7a063fb565dc5e3e024746efc69127859c
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-02-08 00:43:11 +0100

    Add comment for Bash parameter expansion
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- compile.sh
+++ compile.sh
@@ -3,6 +3,8 @@
 APP=$1
 
 # Remove directory if it exists inside container
+# Note that Bash shell parameter expansion ${X##Y} is used to get file basename
+# More on this here: http://stackoverflow.com/questions/2664740/extract-file-basename-without-path-and-extension-in-bash
 docker exec -i uiotaCont bash -c "rm -rf /uiota/staging/${APP##*/}"
 
 # Move dir into the container
